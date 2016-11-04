3dd5d17868d0a949599607d2933fc20a949f1404
Gabriel Júnior Griseli <gabrielgriseli@gmail.com>
2016-11-03 22:06:56 -0200

    Adcionado um ArrayList que guardará os movimentos de entrada e saída de soja. Também criou-se na classe Movimentos os atributos id, data, ope e quant. Além dos métodos depositar e sacar.

--- src/br/com/comercio/modelo/Movimentos.java
+++ src/br/com/comercio/modelo/Movimentos.java
@@ -1,5 +1,32 @@
 package br.com.comercio.modelo;
 
+import java.util.Date;
+
 public class Movimentos {
     
+    private Integer id;
+    private Date data;
+    private char ope;
+    private Integer quant;
+    
+    public Integer depositar(Integer id, Date data, char ope, Integer quant, Integer saldo){
+        this.id = id;
+        this.data = data;
+        this.ope = ope;
+        this.quant = quant;
+        
+        return saldo + quant;
+        ///testar se a pessoa tentar depositar um numero negativo de sacas
+    }
+    
+    public Integer sacar(Integer id, Date data, char ope, Integer quant, Integer saldo){
+        this.id = id;
+        this.data = data;
+        this.ope = ope;
+        this.quant = quant;
+        
+        return saldo - quant;
+        ///testar se a pessoa pode sacar o valor
+        ///tentar sacar o valor em dinheiro e não em sacos de soja
+    }
 }
