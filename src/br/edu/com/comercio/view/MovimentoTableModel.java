bcfa49f2d5a6778c2df093a12e6a2d350f01011b
Gabriel Júnior Griseli <gabrielgriseli@gmail.com>
2016-11-20 11:28:36 -0200

    Incerido a funcionalidade de gravar um movimento. Criado o TableModel.

---
+++ src/br/edu/com/comercio/view/MovimentoTableModel.java
@@ -0,0 +1,59 @@
+
+package br.edu.com.comercio.view;
+
+import br.com.comercio.modelo.Movimento;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MovimentoTableModel extends AbstractTableModel{
+
+     private List<Movimento> movimentos;
+    
+    public MovimentoTableModel(List<Movimento> movimentos) {
+        this.movimentos = movimentos;
+    }
+
+    public void setMovimentos(List<Movimento> movimentos) {
+        this.movimentos = movimentos;
+    }
+
+    @Override
+    public int getRowCount() {
+        return movimentos.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 5;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Movimento movimento = movimentos.get(rowIndex);
+        switch (columnIndex) {
+            case 0: return movimento.getId();
+            case 1: return movimento.getData();
+            case 2: return movimento.getOpe();
+            case 3: return movimento.getQuant();
+            default:
+                throw new AssertionError();
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        switch (column) {
+            case 0: return "Id";
+            case 1: return "Data";
+            case 2: return "Operação";
+            case 3: return "Quantidade";
+            default:
+                throw new AssertionError();
+        }
+    }
+    
+}
