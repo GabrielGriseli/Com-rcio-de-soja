07fe519f23ff1128d77b87bbbfd8f448a7bfd65b
Gabriel Júnior Griseli <gabrielgriseli@gmail.com>
2016-11-20 18:04:50 -0200

    Salvar DataBase.

---
+++ src/br/com/comercio/database/DataBase.java
@@ -0,0 +1,12 @@
+package br.com.comercio.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBase {
+    public static Connection getConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/DB", "usuario", "senha");
+        return conn;
+    }
+}
