505c2db52467196075492bfe75973626a6701d82
Tobias Heitor <tobias.goettert@compasso.com.br>
2020-09-25 10:42:48 -0300

    Ajustes no código.

--- src/br/com/comercio/modelo/QuantidadeInvalidaException.java
+++ src/br/com/comercio/modelo/QuantidadeInvalidaException.java
@@ -1,10 +1,12 @@
 package br.com.comercio.modelo;
 
 
-public class QuantidadeInvalidaException extends Exception{
-    String msg;
+public class QuantidadeInvalidaException extends Exception {
+
+    private static final long serialVersionUID = -7646936181105482626L;
+    private String msg;
     
-    QuantidadeInvalidaException(String msg){
+    QuantidadeInvalidaException(String msg) {
       super(msg);
       this.msg = msg;
     }
